39c0283c74945c19573a197ccc8766b0bc321ba2
vicotroshenko <otroshenko.vl@gmail.com>
2024-10-26 16:13:05 +0300

    feat: add svg components

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -4,10 +4,7 @@ import CustomButton from '../components/CustomButton/CustomButton.component';
 import CustomLink from '../components/CustomLink/CustomLink.component';
 
 function App() {
-  return (
-    <main>
-    </main>
-  );
+  return <main></main>;
 }
 
 export default App;
