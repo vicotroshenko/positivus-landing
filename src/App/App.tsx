d81927b6810137a58dc2f2b78b1b79b3900dc0d2
vicotroshenko <otroshenko.vl@gmail.com>
2024-10-26 13:07:23 +0300

    feat: Add custom button

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,12 +1,10 @@
 import React from 'react';
+import CustomButton from '../components/CustomButton/CustomButton.component';
 
 function App() {
   return (
     <main>
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates, sed
-      at facilis deleniti vero quasi repudiandae laudantium aspernatur minus
-      ducimus reiciendis beatae eligendi ad? Vero, placeat dolores? Ea, optio
-      reprehenderit.
+      <CustomButton variant='OUTLINED'/>
     </main>
   );
 }
---
+++ src/components/CustomButton/CustomButton.component.tsx
@@ -0,0 +1,42 @@
+import clsx from 'clsx';
+import React from 'react';
+
+import styles from './CustomButton.module.css';
+
+enum ButtonsStyle {
+  PRIMARY = 'PRIMARY',
+  OUTLINED = 'OUTLINED',
+  SECONDARY = 'SECONDARY',
+}
+
+interface CustomButtonProps
+  extends Omit<React.ButtonHTMLAttributes<HTMLButtonElement>, 'className'> {
+  children?: React.ReactNode;
+  variant: keyof typeof ButtonsStyle;
+  onClick?: () => void;
+  onSubmit?: () => void;
+}
+const CustomButton: React.FC<CustomButtonProps> = ({
+  children = null,
+  variant,
+  onClick,
+  onSubmit,
+  ...props
+}) => {
+  return (
+    <button
+      {...props}
+      onClick={onClick}
+      onSubmit={onSubmit}
+      className={clsx(styles.button, {
+        [styles.outlined]: ButtonsStyle.OUTLINED === variant,
+        [styles.primary]: ButtonsStyle.PRIMARY === variant,
+        [styles.secondary]: ButtonsStyle.SECONDARY === variant,
+      })}
+    >
+      CustomButton
+    </button>
+  );
+};
+
+export default CustomButton;
