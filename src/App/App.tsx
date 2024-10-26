01817f55c59411a46de93f05d24fc26f9c29d67e
vicotroshenko <otroshenko.vl@gmail.com>
2024-10-26 12:05:52 +0300

    feat: Add base styles, variables, fonts

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -3,7 +3,10 @@ import React from 'react';
 function App() {
   return (
     <main>
-
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates, sed
+      at facilis deleniti vero quasi repudiandae laudantium aspernatur minus
+      ducimus reiciendis beatae eligendi ad? Vero, placeat dolores? Ea, optio
+      reprehenderit.
     </main>
   );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+
 import App from './App/App';
+import './assets/styles/index.css';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(
